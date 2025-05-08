bf371c928e91428e6353c0f088002af15e134aa8
quang.nguyentruong <quang.nguyentruong@buymed.asia>
2025-05-08 14:24:47 +0700

    chore: clean up main

--- cmd/go-rkinetics-service/main.go
+++ cmd/go-rkinetics-service/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"strings"
 
@@ -50,41 +49,20 @@ func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
 
 func getConfig() (*conf.Bootstrap, error) {
 	viper.SetConfigFile(flagconf)
-	// Set default values for the configuration parameters
 	viper.SetDefault("server.env", "development")
 	viper.SetDefault("server.log_level", "debug")
-	// set config and read
 	viper.SetConfigType("yaml")
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
 	}
-	// merge with env config
 	viper.SetEnvPrefix("APP")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
-	// Explicitly bind environment variables
-	viper.BindEnv("data.mongo.address", "APP_DATA_MONGO_ADDRESS")
-	viper.BindEnv("data.mongo.username", "APP_DATA_MONGO_USERNAME")
-	viper.BindEnv("data.mongo.password", "APP_DATA_MONGO_PASSWORD")
-	viper.BindEnv("data.mongo.auth_db", "APP_DATA_MONGO_AUTH_DB")
-	viper.BindEnv("data.mongo.db_name", "APP_DATA_MONGO_DB_NAME")
-	viper.BindEnv("data.s3.access_key", "APP_DATA_S3_ACCESS_KEY")
-	viper.BindEnv("data.s3.secret_key", "APP_DATA_S3_SECRET_KEY")
-	viper.BindEnv("data.s3.region", "APP_DATA_S3_REGION")
-	viper.BindEnv("data.s3.bucket", "APP_DATA_S3_BUCKET")
-
-	// Log all Viper keys for debugging
 	for _, key := range viper.AllKeys() {
-		fmt.Printf("Viper key: %s, value: %v", key, viper.Get(key))
+		viper.Set(key, viper.Get(key))
 	}
 
-	// override with ENV vars
-	// for _, key := range viper.AllKeys() {
-	// 	viper.Set(key, viper.Get(key))
-	// }
-
-	// unmarshal to config object
 	var cfg conf.Bootstrap
 	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
@@ -104,27 +82,6 @@ func main() {
 		"trace.id", tracing.TraceID(),
 		"span.id", tracing.SpanID(),
 	)
-
-	// logger := log.With(log.NewStdLogger(os.Stdout),
-	// 	"ts", log.DefaultTimestamp,
-	// 	"caller", log.DefaultCaller,
-	// 	"service.id", id,
-	// 	"service.name", Name,
-	// 	"service.version", Version,
-	// 	"trace.id", tracing.TraceID(),
-	// 	"span.id", tracing.SpanID(),
-	// )
-
-	// c := config.New(
-	// 	config.WithSource(
-	// 		file.NewSource(flagconf),
-	// 	),
-	// )
-	// defer c.Close()
-
-	// if err := c.Load(); err != nil {
-	// 	panic(err)
-	// }
 
 	bc, err := getConfig()
 	if err != nil {
